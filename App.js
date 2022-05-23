8bef2cfaaa117b442cb5e7d7416d47d25e93df5a
enismusliu <enismusliu.work@gmail.com>
2022-05-23 12:31:19 +0200

    commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+const YourApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{ uri: 'https://busulla.com/' }}
+      style={{marginTop: 30}}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default YourApp;
